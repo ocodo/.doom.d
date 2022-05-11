6898353616771b17544707e58010cee048a2d524
Jason Milkins <jasonm23@gmail.com>
2022-05-11 10:34:38 +0700

    Initial commit of .doom.d
    - Added a few themes and google-this
    - Evil mode switched off

---
+++ custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("94146ac747852749e9444b184eb1e958f0e546072f66743929a05c3af62de473" "dcddb6fb811573575e3eafa85bf1b22af61edbea8d3968a7b53d9a4776595811" "1704976a1797342a1b4ea7a75bdbb3be1569f4619134341bd5a4c1cfb16abad4" "7a7b1d475b42c1a0b61f3b1d1225dd249ffa1abb1b7f726aec59ac7ca3bf4dae" "1d5e33500bc9548f800f9e248b57d1b2a9ecde79cb40c0b1398dec51ee820daf" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
