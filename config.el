80c905071d70987478a1d5d42069f754e1ae7794
Jason Milkins <jasonm23@gmail.com>
2022-05-15 01:29:52 +0700

    Try re-enabling SauceCodePro Nerd Font (light)

--- config.el
+++ config.el
@@ -28,6 +28,11 @@
 ;; refresh your font settings. If Emacs still can't find your font, it likely
 ;; wasn't installed correctly. Font issues are rarely Doom issues!
 
+(setq doom-font
+      (font-spec
+       :family "SauceCodePro Nerd Font"
+       :weight 'light))
+
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
