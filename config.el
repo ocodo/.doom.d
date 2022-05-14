d37d76f7ad7e49134f4dd0680c101c577516e007
jasonm23 <jasonm23@gmail.com>
2022-05-14 14:37:28 +0700

    Switch to Creamsody theme + SauceCodePro Nerd Font

--- config.el
+++ config.el
@@ -27,11 +27,14 @@
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
 ;; refresh your font settings. If Emacs still can't find your font, it likely
 ;; wasn't installed correctly. Font issues are rarely Doom issues!
+(setq doom-font (font-spec
+                 :family "SauceCodePro Nerd Font"
+                 :weight 'semilight))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-henna)
+(setq doom-theme 'creamsody)
 
 ;; Use flex completion style (fuzzy match)
 (add-to-list 'completion-styles 'flex)
--- packages.el
+++ packages.el
@@ -10,6 +10,8 @@
 
 (package! google-this)
 (package! iedit)
+(package! creamsody-theme)
+(package! autothemer)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
