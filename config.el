6918184e001d391a196e0266c2ec6d3c90a78b15
jasonm23 <jasonm23@gmail.com>
2022-06-12 12:58:20 +0700

    "Fix" font (no idea why Emacs doesn't see extralight when MacOS does!??)

--- config.el
+++ config.el
@@ -58,7 +58,7 @@
 (setq doom-font
       (font-spec
        :family "SauceCodePro Nerd Font"
-       :weight 'extralight))
+       :weight 'light))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
