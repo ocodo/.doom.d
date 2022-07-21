6379fbfff8646c512097db9cc867fc42392111d2
Jason Milkins <jasonm23@gmail.com>
2022-07-21 17:58:16 +0700

    Add orangey bits splash

--- config.el
+++ config.el
@@ -14,7 +14,7 @@
 (add-hook 'doom-load-theme-hook #'set-fancy-splash)
 
 (defun set-fancy-splash ()
-  (setq fancy-splash-image "~/.doom.d/doom-vapourwave.png")
+  (setq fancy-splash-image "~/.doom.d/doom-orangeybits.png")
   (add-hook! 'after-setting-font-hook (+doom-dashboard-reload 'force)))
 
 (setq doom-theme 'orangey-bits)
