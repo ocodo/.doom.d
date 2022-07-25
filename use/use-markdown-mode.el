9110055e38203003e17d70af5556737cae512525
Jason Milkins <jasonm23@gmail.com>
2022-07-25 21:03:41 +0700

    Fix CSS selection for markdown-soma

--- use/use-markdown-mode.el
+++ use/use-markdown-mode.el
@@ -5,10 +5,11 @@
   :config
   (use-package! markdown-soma
     :config (setq markdown-soma-custom-css
-                  "~/workspace/soma/styles/lopped-off-dark-subtle.css"
+                  (markdown-soma--css-pathname-from-builtin-name "markdown-soma-dark-subtle")
                   markdown-soma-highlight-theme
                   "atelier-plateau.dark")
 
+    (bind-key "C-c S b"   #'markdown-soma-select-builtin-css 'markdown-mode-map)
     (bind-key "C-c S c"   #'markdown-soma-select-css-file 'markdown-mode-map)
     (bind-key "C-c S h"   #'markdown-soma-select-highlight-theme 'markdown-mode-map)
     (bind-key "C-c S s"   #'markdown-soma-mode 'markdown-mode-map)
