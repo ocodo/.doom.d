905ca2cfd8ce8ff2e181fc457d727ab419742836
Jason Milkins <jasonm23@gmail.com>
2022-07-26 22:20:03 +0700

    Add ruby-send... key bindings

---
+++ use/use-ruby.el
@@ -0,0 +1,6 @@
+(use-package! ruby-mode
+  :config
+
+  (bind-key "C-c l e b" #'ruby-send-buffer 'ruby-mode-map)
+  (bind-key "C-c l e d" #'ruby-send-block 'ruby-mode-map)
+  (bind-key "C-c l e r" #'ruby-send-region 'ruby-mode-map))
