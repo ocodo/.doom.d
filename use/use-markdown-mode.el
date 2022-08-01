372c99405015e9615c5abbcdcea58ac94aba0f99
jasonm23 <jasonm23@gmail.com>
2022-08-01 18:08:13 +0700

    Deactivate dart/flutter while dart-mode is MIA
    - Also fix use-markdown

--- use/use-markdown-mode.el
+++ use/use-markdown-mode.el
@@ -2,13 +2,12 @@
 
 ;; Markdown settings
 (use-package! markdown-mode
-  :config
-  (use-package! markdown-soma
-    :config (setq markdown-soma-custom-css (markdown-soma--css-pathname-from-builtin-name "markdown-soma-dark-subtle")
-                  markdown-soma-highlight-theme "atelier-plateau.dark")
+  :config (use-package! markdown-soma
+            :config (setq markdown-soma-custom-css (markdown-soma--css-pathname-from-builtin-name "markdown-soma-dark-subtle")
+                          markdown-soma-highlight-theme "atelier-plateau.dark")
 
-    (bind-key "C-c S b"   #'markdown-soma-select-builtin-css 'markdown-mode-map)
-    (bind-key "C-c S c"   #'markdown-soma-select-css-file 'markdown-mode-map)
-    (bind-key "C-c S h"   #'markdown-soma-select-highlight-theme 'markdown-mode-map)
-    (bind-key "C-c S s"   #'markdown-soma-mode 'markdown-mode-map)
-    (bind-key "C-c S r"   #'markdown-soma-restart 'markdown-mode-map)))
+            (bind-key "C-c S b"   #'markdown-soma-select-builtin-css 'markdown-mode-map)
+            (bind-key "C-c S c"   #'markdown-soma-select-css-file 'markdown-mode-map)
+            (bind-key "C-c S h"   #'markdown-soma-select-highlight-theme 'markdown-mode-map)
+            (bind-key "C-c S s"   #'markdown-soma-mode 'markdown-mode-map)
+            (bind-key "C-c S r"   #'markdown-soma-restart 'markdown-mode-map)))
