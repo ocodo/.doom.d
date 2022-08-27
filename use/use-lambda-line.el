c1fcf6444c3e51fa27ad6c341875e82b72b06222
jasonm23 <jasonm23@gmail.com>
2022-08-27 09:30:17 +0700

    Modeline yakshave

--- use/use-lambda-line.el
+++ use/use-lambda-line.el
@@ -3,15 +3,16 @@
   :custom
   (lambda-line-position 'bottom) ;; Set position of status-line
   (lambda-line-abbrev t) ;; abbreviate major modes
-  (lambda-line-hspace "  ")  ;; add some cushion
+  (lambda-line-hspace "")  ;; add some cushion
   (lambda-line-prefix t) ;; use a prefix symbol
-  (lambda-line-prefix-padding nil) ;; no extra space for prefix
+  (lambda-line-prefix-padding t) ;; no extra space for prefix
   (lambda-line-status-invert nil)  ;; no invert colors
-  (lambda-line-gui-ro-symbol  "  ⨂  ") ;; symbols
-  (lambda-line-gui-mod-symbol "  ⬤  ")
-  (lambda-line-gui-rw-symbol  "  ◯  ")
-  (lambda-line-space-top +.25)  ;; padding on top and bottom of line
-  (lambda-line-space-bottom -.25)
+  (lambda-line-gui-ro-symbol  "  ⊖ ") ;; symbols
+  (lambda-line-gui-mod-symbol "  ⦿ ")
+  (lambda-line-gui-rw-symbol  "  ⭘  ")
+  (lambda-line-vc-symbol  "   ")
+  (lambda-line-space-top 0.45)  ;; padding on top and bottom of line
+  (lambda-line-space-bottom -0.30)
   (lambda-line-symbol-position 0.1) ;; adjust the vertical placement of symbol
   :config
   ;; activate lambda-line
