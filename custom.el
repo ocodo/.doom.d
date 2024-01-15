4a1a9c67491422f58b8d05959cc1e25e1e87812d
Jason Milkins <jasonm23@gmail.com>
2024-01-15 13:27:30 +0700

    Remove custom set faces from custom.el

--- custom.el
+++ custom.el
@@ -4,19 +4,12 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("0d23ecaa8eb7cc7b6c303c484677a6cbb85a7847e1840b1c5cd6b9477f11df9e" default)))
+   '("6c49dc039a835cc165502093ce3ab2a0d6b06f868dfd04727e88b0d8bc252fa0" "2e98350610ed0594dab8c55fcb4b4d781e020bf022dda8e7e23b038208cb6e5e" "56044c5a9cc45b6ec45c0eb28df100d3f0a576f18eef33ff8ff5d32bac2d9700" "0d23ecaa8eb7cc7b6c303c484677a6cbb85a7847e1840b1c5cd6b9477f11df9e" default))
+ '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'"))
 (put 'projectile-ripgrep 'disabled nil)
 (put 'customize-face 'disabled nil)
 (put 'customize-set-variable 'disabled nil)
 (put 'customize-group 'disabled nil)
 (put 'customize-apropos-faces 'disabled nil)
 (put 'customize-rogue 'disabled nil)
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(mode-line ((t (:family "Helvetica Neue" :weight ultra-light))))
- '(mode-line-active ((t (:family "Helvetica Neue" :weight ultra-light))))
- '(mode-line-inactive ((t (:family "Helvetica Neue" :weight ultra-light)))))
 (put 'narrow-to-region 'disabled nil)
