beba7a775a143a907f4ef70f3e3279915f98b076
JasonM <jasonm23@gmail.com>
2024-06-01 10:37:40 +0700

    Add OcodoMono Nerd Font to favorites (better symbol support and spacingdefined by Nerd Font Patcher)

--- plugins/ocodo-font-favorites.el
+++ plugins/ocodo-font-favorites.el
@@ -9,6 +9,7 @@
                                 ("SauceCodePro Nerd Light"      (:family "SauceCodePro Nerd Font" :weight semi-light))
                                 ("SauceCodePro Nerd Regular"    (:family "SauceCodePro Nerd Font" :weight normal))
                                 ("OcodoMono"                    (:family "OcodoMono" :weight thin))
+                                ("OcodoMono Nerd Font"          (:family "OcodoMono Nerd Font" :weight thin))
                                 ("Input Mono Regular"           (:family "Input Mono" :weight normal))
                                 ("Input Mono Light"             (:family "Input Mono" :weight semi-light))
                                 ("APL385 Unicode"               (:family "APL385 Unicode" :weight normal))
@@ -16,6 +17,7 @@
                                 ("IBM Plex Mono ExtraLight"     (:family "IBM Plex Mono" :weight light))
                                 ("IBM Plex Mono Light"          (:family "IBM Plex Mono" :weight semi-light))
                                 ("IBM Plex Mono Regular"        (:family "IBM Plex Mono" :weight normal))))
+
           (variable-pitch-fonts '(("Helvetica"                    (:family "Helvetica" :weight normal))
                                   ("Helvetica Neue Regular"       (:family "Helvetica Neue" :weight normal))
                                   ("Helvetica Neue Light"         (:family "Helvetica Neue" :weight light))
@@ -29,21 +31,34 @@
                                   ("Gill Sans Light"              (:family "Gill Sans" :weight light))
                                   ("Avenir Next Ultralight"       (:family "Avenir Next" :weight thin))
                                   ("Avenir Next Regular"          (:family "Avenir Next" :weight normal))))
+
           (modeline-fonts (append default-mono-fonts variable-pitch-fonts))
 
           (selected-fonts (--map (assoc
-                                  (completing-read (format "Select %s [nil to skip]:" (car it)) (-map (lambda (that) (car that)) it))
-                                  it)
-                           (list default-mono-fonts
-                                 variable-pitch-fonts
-                                 modeline-fonts))))
+                                   (completing-read (format
+                                                     "Select %s Font [nil to skip]:"
+                                                     (plist-get it :name))
 
-        (when (car selected-fonts) (setq doom-font (apply 'font-spec (cadr (car selected-fonts)))))
-        (when (cadr selected-fonts) (setq doom-variable-pitch-font (apply 'font-spec (cadr (cadr selected-fonts)))))
-        (when (caddr selected-fonts)
-         (let* ((spec (cadr (caddr selected-fonts)))
-                (font (plist-get spec :family))
-                (weight (plist-get spec :weight)))
-            (set-face-attribute 'mode-line nil :family font :weight weight)
-            (set-face-attribute 'mode-line-inactive nil :family font :weight weight)))
-       (doom/reload-font))))
+                                                    (mapcar
+                                                     (lambda (that) that)
+                                                     (plist-get it :fonts)))
+                                   (plist-get it :fonts))
+
+                              (list (list :name "Default Monospaced" :fonts default-mono-fonts)
+                                    (list :name "Default Variable Pitch" :fonts variable-pitch-fonts)
+                                    (list :name "Modeline" :fonts modeline-fonts))))
+          (selected-doom-mono-font (cadr (nth 0 selected-fonts)))
+          (selected-doom-variable-font (cadr (nth 1 selected-fonts)))
+          (selected-doom-modeline-font (cadr (nth 2 selected-fonts))))
+
+        (when selected-doom-mono-font
+          (setq doom-font (apply 'font-spec selected-doom-mono-font)))
+        (when selected-doom-variable-font
+          (setq doom-variable-pitch-font (apply 'font-spec selected-doom-variable-font)))
+        (when selected-doom-modeline-font
+          (let* ((spec selected-doom-modeline-font)
+                 (font (plist-get spec :family))
+                 (weight (plist-get spec :weight)))
+               (set-face-attribute 'mode-line nil :family font :weight weight)
+               (set-face-attribute 'mode-line-inactive nil :family font :weight weight)))
+    (doom/reload-font))))
