b44d09025aeef5382fc3dd1afbbb43607f995ef4
JasonM <jasonm23@gmail.com>
2024-06-21 20:01:43 +0700

    Make ellama use the local server or nebula if the ollama server is not running locally.

--- use/use-ellama.el
+++ use/use-ellama.el
@@ -1,16 +1,31 @@
-(use-package ellama
-  :init
-  (setopt ellama-keymap-prefix "C-c =")
-  (require 'llm-ollama)
-  (setopt ellama-provider
-	  (make-llm-ollama
-           :host "nebula"
-	   :chat-model "superdrew100/llama3-abliterated:latest"
-	   :embedding-model "superdrew100/llama3-abliterated:latest"))
-  ;; Naming new sessions with llm
-  (setopt ellama-naming-provider
-	  (make-llm-ollama
-           :host "nebula"
-	   :chat-model "superdrew100/llama3-abliterated:latest"
-	   :embedding-model "superdrew100/llama3-abliterated:latest"))
-  (setopt ellama-naming-scheme 'ellama-generate-name-by-llm))
+(require 's)
+
+(defun ocodo/local-ollama-server-p ()
+  "Check for local Ollama LLM server."
+  (let ((netstat-result
+         (s-chomp (shell-command-to-string "netstat -a | grep 11434"))))
+    (s-contains-p "localhost:11434" netstat-result)))
+
+(let
+
+    ((host
+      (if (ocodo/local-ollama-server-p)
+          "localhost"
+        "nebula")))
+
+  (use-package ellama
+    :init
+    (setopt ellama-keymap-prefix "C-c =")
+    (require 'llm-ollama)
+    (setopt ellama-provider
+      (make-llm-ollama
+       :host host
+       :chat-model "superdrew100/llama3-abliterated:latest"
+       :embedding-model "superdrew100/llama3-abliterated:latest"))
+    ;; Naming new sessions with llm
+    (setopt ellama-naming-provider
+            (make-llm-ollama
+             :host host
+             :chat-model "superdrew100/llama3-abliterated:latest"
+             :embedding-model "superdrew100/llama3-abliterated:latest"))
+    (setopt ellama-naming-scheme 'ellama-generate-name-by-llm)))
